5762be90405641c2cd98b874478087f6611c9546
salihRogo <salih.rogo@stu.ibu.edu.ba>
2025-04-18 00:26:21 +0200

    /shops route completed

--- backend/index.php
+++ backend/index.php
@@ -1,12 +1,11 @@
 <?php
 
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 require_once __DIR__ . '/rest/services/ShopsService.class.php';
 
 Flight::register('shopsService', 'ShopsService');
 
-// require 'rest/routes/middleware_routes.php';
-require __DIR__ . '/rest/routes/shops_routes.php';
+require_once __DIR__ . '/rest/routes/shops_routes.php';
 
 Flight::start();
