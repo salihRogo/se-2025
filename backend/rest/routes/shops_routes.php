82915ae5db6125a8302c06e4dca818ec58caab18
ljirna <mirna.ljiljic@stu.ibu.edu.ba>
2025-05-07 14:03:33 +0200

    shops crud done

--- backend/rest/routes/shops_routes.php
+++ backend/rest/routes/shops_routes.php
@@ -2,16 +2,24 @@
 
 require_once __DIR__ . '/../services/ShopsService.class.php';
 
-Flight::route('GET /shops', function() {
-    Flight::json(Flight::shopsService() -> get_all_shops());
+Flight::route('GET /shops', function () {
+    Flight::json(Flight::shopsService()->get_all_shops());
 });
 
-Flight::route('GET /nebitno', function() {
-    echo "Ovo radi";
+Flight::route('GET /shops/@id', function ($id) {
+    Flight::json(Flight::shopsService()->get_shop_by_id($id));
 });
 
-Flight::route('/', function() {
-    echo "Ovo valjda radi";
+Flight::route('POST /shop', function(){
+    $shops = Flight::request()->data->getData();
+    Flight::json(Flight::shopsService()->add_shop($shops));
 });
 
-Flight::start();
+Flight::route('DELETE /shop/@id', function($id){
+    Flight::json(Flight::shopsService()->delete_shop($id));
+});
+
+Flight::route('PUT /shop/@id', function($id) {
+    $shops = Flight::request()->data->getData();
+    Flight::json(Flight::shopsService()->update_shop($id, $shops));
+});
--- backend/rest/services/ShopsService.class.php
+++ backend/rest/services/ShopsService.class.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\Yaml\Yaml;
+
 require_once __DIR__ . '/../dao/ShopsDao.class.php';
 require_once 'BaseService.class.php';
 
@@ -9,12 +11,32 @@ class ShopsService extends BaseService
 
     public function __construct()
     {
-        $this -> shopsDao = new ShopsDao();
-        parent::__construct($this -> shopsDao);
+        $this->shopsDao = new ShopsDao();
+        parent::__construct($this->shopsDao);
     }
 
     public function get_all_shops()
     {
-        return $this -> shopsDao -> get_all_shops();
+        return $this->shopsDao->get_all_shops();
+    }
+
+    public function get_shop_by_id($id)
+    {
+        return $this->shopsDao->get_shop_by_id($id);
+    }
+
+    public function add_shop($shops)
+    {
+        return $this->shopsDao->add_shop($shops);
+    }
+
+    public function delete_shop($id)
+    {
+        return $this->shopsDao->delete_shop($id);
+    }
+
+    public function update_shop($id, $shops)
+    {
+        return $this->shopsDao->update_shop($id, $shops);
     }
 }
